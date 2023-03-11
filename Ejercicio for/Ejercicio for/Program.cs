f9ded31396e1f82d081de63eb4b2f6a692ce970c
Gloribel010 <125325837+Gloribel010@users.noreply.github.com>
2023-03-10 20:22:43 -0600

    Add files via upload

---
+++ Ejercicio for/Ejercicio for/Program.cs
@@ -0,0 +1,23 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!"
+
+
+Console.WriteLine("Triangulo rectangulo");
+class For
+{
+    static void Main(string[] args)
+    {
+        Console.Write("ingresar un numero entero");
+        int altura = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= altura; i++)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
